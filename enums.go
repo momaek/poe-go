f9dd7749bbc7497c5ffbcd8711f68a654013a442
momaek <momaek17@gmail.com>
2023-07-31 23:50:28 +0800

    update client

---
+++ enums.go
@@ -0,0 +1,13 @@
+package poego
+
+// SourceTypeChatInput
+type SourceType string
+
+const SourceTypeChatInput SourceType = "chat_input"
+
+const (
+	GraphQLPOSTURL = "https://poe.com/api/gql_POST"
+	GraphQLRecvURL = "https://poe.com/api/receive_POST"
+	HomeURL        = "https://poe.com"
+	SettingsURL    = "https://poe.com/api/settings"
+)
